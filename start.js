1d3fbb5951d5979a60c7cb1470a61826c93d34e7
NCN Dev <ncntestdev@gmail.com>
2022-06-21 09:01:37 +0000

    nodemon

--- start.js
+++ start.js
@@ -1,4 +1,7 @@
 var http = require('http');
+
+console.log( "hello rajat" );
+
  http.createServer(function (req, res) {
    res.writeHead(200, {'Content-Type': 'text/html'});
    res.end('Hello World!  rajat');
